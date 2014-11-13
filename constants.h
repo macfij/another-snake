25dac8c8cf41a8c87c5de75a93cb3ea2d0a7ffc2
Maciej Fijałkowski <macfij7@wp.pl>
2014-11-13 17:47:58 +0100

    Removed old files.

--- constants.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-
-const int SCREEN_BPP = 32;
-//const int FRAMES_PER_SECOND = 20;
-const int SNAKE_LENGTH = 25;
-const int SNAKE_CELL_SEPARATION = 1;
-
-#endif
